b1bc8f9766017af93ce23728577fc529f1fb99c0
Kendall Brown <kendallbrwn@gmail.com>
2021-05-09 17:45:04 -0600

    Implemented 'get_probabilities' in 'Event_Two_Dependencies'

--- src/Event_One_Dependency.java
+++ src/Event_One_Dependency.java
@@ -16,7 +16,6 @@ public class Event_One_Dependency extends Event {
 	
 	@Override
 	public double[] get_probabilities() {
-		// TODO Auto-generated method stub
 		double[] probabilities;
 		
 		if (this.is_set) {
@@ -25,8 +24,10 @@ public class Event_One_Dependency extends Event {
 				probabilities = new double[1];
 				
 				if (this.is_true) {
+					//event is true
 					probabilities[0] = this.probability_table.get(dependency.get_is_true());
 				} else {
+					//event is false
 					probabilities[0] = 1 - this.probability_table.get(dependency.get_is_true());
 				}
 				
@@ -35,10 +36,14 @@ public class Event_One_Dependency extends Event {
 				probabilities = new double[2];
 				
 				if (this.is_true) {
+					//event is true / dependency is true
 					probabilities[0] = this.probability_table.get(true);
+					//event is true / dependency is false
 					probabilities[1] = this.probability_table.get(false);
 				} else {
+					//event is false / dependency is true
 					probabilities[0] = 1 - this.probability_table.get(true);
+					//event is false / dependency is false
 					probabilities[1] = 1 - this.probability_table.get(false);
 				}
 			}
@@ -48,16 +53,22 @@ public class Event_One_Dependency extends Event {
 			if (this.dependency.get_is_set()) {
 				probabilities = new double[2];
 				
+				// event is true / dependency is known
 				probabilities[0] = this.probability_table.get(dependency.get_is_true());
+				// event is false / dependency is known
 				probabilities[1] = 1 - this.probability_table.get(dependency.get_is_true());
 
 			} else {
 				//probabilities of this event if neither it nor its dependency is known
 				probabilities = new double[4];
 				
+				// event is true / dependency is true
 				probabilities[0] = this.probability_table.get(true);
+				// event is false / dependency is true
 				probabilities[1] = 1 - this.probability_table.get(true);
+				// event is true / dependency is false
 				probabilities[2] = this.probability_table.get(false);
+				// event is false / dependency is false
 				probabilities[3] = 1 - this.probability_table.get(false);
 				
 			}
--- src/Event_Two_Dependencies.java
+++ src/Event_Two_Dependencies.java
@@ -48,6 +48,7 @@ public class Event_Two_Dependencies extends Event{
 				//probabilities of this event if it and only one of its dependencies are known
 				probabilities = new double[2];
 				
+				//dependency 1 is known
 				if (this.dependencies[0].get_is_set()) {
 					Boolean[] key = {this.dependencies[0].get_is_true(), true};
 					
@@ -59,7 +60,7 @@ public class Event_Two_Dependencies extends Event{
 						key[1] = false;
 						probabilities [1] = this.probability_table.get(key);
 					} else {
-						//event is true / dependency 1 is known / dependency 2 is true
+						//event is false / dependency 1 is known / dependency 2 is true
 						probabilities [0] = 1 - this.probability_table.get(key);
 						
 						//event is false / dependency 1 is known / dependency 2 is false
@@ -67,11 +68,152 @@ public class Event_Two_Dependencies extends Event{
 						probabilities [1] = 1 - this.probability_table.get(key);
 					}
 					
+				} else {
+					//dependency 2 is known
+					Boolean[] key = {true, this.dependencies[1].get_is_true()};
+					
+					if (this.is_true) {
+						//event is true / dependency 1 is true / dependency 2 is known
+						probabilities [0] = this.probability_table.get(key);
+						
+						//event is true / dependency 1 is false / dependency 2 is known
+						key[0] = false;
+						probabilities [1] = this.probability_table.get(key);
+					} else {
+						//event is false / dependency 1 is true / dependency 2 is known
+						probabilities [0] = 1 - this.probability_table.get(key);
+						
+						//event is false / dependency 1 is false / dependency 2 is known
+						key[0] = false;
+						probabilities [1] = 1 - this.probability_table.get(key);
+					}
+					
 				}
+				
+			} else {
+				//probabilities of this event if it is known and none of its dependencies are known
+				probabilities = new double[4];
+				Boolean[] key = {true, true};
+				
+				if (this.is_true) {
+					//Event is true / dependency 1 is true / dependency 2 is true
+					probabilities [0] = this.probability_table.get(key);
+					
+					//Event is true / dependency 1 is true / dependency 2 is false
+					key[1] = false;
+					probabilities [1] = this.probability_table.get(key);
+					
+					//Event is true / dependency 1 is false / dependency 2 is true
+					key[0] = false;
+					key[1] = true;
+					probabilities [2] = this.probability_table.get(key);
+					
+					//Event is true / dependency 1 is false / dependency 2 is false
+					key[1] = false;
+					probabilities [3] = this.probability_table.get(key);
+				} else {
+					//Event is false / dependency 1 is true / dependency 2 is true
+					probabilities [0] = 1 - this.probability_table.get(key);
+					
+					//Event is false / dependency 1 is true / dependency 2 is false
+					key[1] = false;
+					probabilities [1] = 1 - this.probability_table.get(key);
+					
+					//Event is false / dependency 1 is false / dependency 2 is true
+					key[0] = false;
+					key[1] = true;
+					probabilities [2] = 1 - this.probability_table.get(key);
+					
+					//Event is false / dependency 1 is false / dependency 2 is false
+					key[1] = false;
+					probabilities [3] = 1 - this.probability_table.get(key);
+				}
+				
 			}
+			
+		} else {
+			//probabilities of this event if it is not known and its dependencies are known
+			if (this.dependencies[0].get_is_set() && this.dependencies[1].get_is_set()) {
+				probabilities = new double[2];
+				Boolean[] key = {this.dependencies[0].get_is_true(), this.dependencies[1].get_is_true()};
+				
+				//Event is true / dependency 1 is known / dependency 2 is known
+				probabilities[0] = this.probability_table.get(key);
+				//Event is false / dependency 1 is known / dependency 2 is known
+				probabilities[1] = 1 - this.probability_table.get(key);
+				
+			} else if (this.dependencies[0].get_is_set() || this.dependencies[1].get_is_set()) {
+				//probabilities of this event if it is not known and only one of its dependencies are known
+				probabilities = new double[4];
+				
+				//dependency 1 is known
+				if (this.dependencies[0].get_is_set()) {
+					Boolean[] key = {this.dependencies[0].get_is_true(), true};
+					
+
+					//event is true / dependency 1 is known / dependency 2 is true
+					probabilities [0] = this.probability_table.get(key);
+					//event is false / dependency 1 is known / dependency 2 is true
+					probabilities [1] = 1 - this.probability_table.get(key);
+					
+					//event is true / dependency 1 is known / dependency 2 is false
+					key[1] = false;
+					probabilities [2] = this.probability_table.get(key);
+					//event is false / dependency 1 is known / dependency 2 is false
+					probabilities [3] = 1 - this.probability_table.get(key);
+
+
+				} else {
+					//dependency 2 is known
+					Boolean[] key = {true, this.dependencies[1].get_is_true()};
+					
+					//event is true / dependency 1 is true / dependency 2 is known
+					probabilities [0] = this.probability_table.get(key);
+					//event is false / dependency 1 is true / dependency 2 is known
+					probabilities [1] = 1 - this.probability_table.get(key);
+					
+					//event is true / dependency 1 is false / dependency 2 is known
+					key[0] = false;
+					probabilities [2] = this.probability_table.get(key);
+					//event is false / dependency 1 is false / dependency 2 is known
+					probabilities [3] = 1 - this.probability_table.get(key);
+					
+				}
+				
+			} else {
+				//probabilities of this event if none of its dependencies are known
+				probabilities = new double[8];
+				Boolean[] key = {true, true};
+				
+				//Event is true / dependency 1 is true / dependency 2 is true
+				probabilities [0] = this.probability_table.get(key);
+				//Event is true / depencency 1 is true / dependency 2 is true
+				probabilities [1] = 1 - this.probability_table.get(key);
+
+				//Event is true / dependency 1 is true / dependency 2 is false
+				key[1] = false;
+				probabilities [2] = this.probability_table.get(key);
+				//Event is false / dependency 1 is true / dependency 2 is false
+				probabilities[3] = 1 - this.probability_table.get(key);
+
+				//Event is true / dependency 1 is false / dependency 2 is true
+				key[0] = false;
+				key[1] = true;
+				probabilities [4] = this.probability_table.get(key);
+				//Event is false / dependency 1 is false / dependency 2 is true
+				probabilities [5] = 1 - this.probability_table.get(key);
+
+				//Event is true / dependency 1 is false / dependency 2 is false
+				key[1] = false;
+				probabilities [6] = this.probability_table.get(key);
+				//Event is false / dependency 1 is false / dependency 2 is false
+				probabilities [7] = 1 - this.probability_table.get(key);
+
+			}
+			
 		}
 		
-		return null;
+		return probabilities;
 	}
 
 }
