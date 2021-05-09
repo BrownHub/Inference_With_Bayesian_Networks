8c4eaa1be6045ae21c16bb87d2138e810ed1bfd8
Kendall Brown <kendallbrwn@gmail.com>
2021-05-08 22:12:21 -0600

    Initial commit

---
+++ src/Bayesian_Network.java
@@ -0,0 +1,9 @@
+
+public class Bayesian_Network {
+
+	public double compute_probability(boolean b, boolean e, boolean a, boolean j, boolean m) {
+		double probability = 0.0;
+		
+		return probability;
+	}
+}
---
+++ src/Event.java
@@ -0,0 +1,10 @@
+import java.util.Map;
+
+public abstract class Event {
+	private boolean event_true_false;
+	private boolean set;
+	private boolean given;
+	private Map<String, Double> probabilities;
+	
+
+}
